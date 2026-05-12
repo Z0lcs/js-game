1560039f910b1cad66294f4676abc95cb8742db3
herczegAkos25 <akos.herczeg25@gmail.com>
2026-05-12 21:48:49 +0200

    kvíz - végtelen random kérdések mechanika

--- JS/kviz.js
+++ JS/kviz.js
@@ -1,43 +1,49 @@
 var kerdesek = [
-  ;
+];
 
 var sorszam = Math.floor(Math.random() * kerdesek.length);
-var pontok = 0; 
+var pontok = 0;
+var joValasz = true;
 
 function kerdesBetoltes() {
     var aktualis = kerdesek[sorszam];
     
-    document.getElementById("kerdes-szoveg").innerHTML = aktualis[0];
+    var kerdesElem = document.getElementById("kerdes-szoveg uveges");
+    if (kerdesElem) {
+        kerdesElem.innerHTML = aktualis[0];
+    }
+    
     document.getElementById("b0").innerHTML = aktualis[1];
     document.getElementById("b1").innerHTML = aktualis[2];
     document.getElementById("b2").innerHTML = aktualis[3];
     document.getElementById("b3").innerHTML = aktualis[4];
 }
-
 function valaszEllenorzese(szam) {
-    var helyesValasz = kerdesek[sorszam][5];
+    var helyesValaszIndex = kerdesek[sorszam][5];
 
-    if (szam == helyesValasz) {
+    if (szam == helyesValaszIndex) {
         alert("Helyes! Ügyes kacsa vagy! 🦆");
-        pontok = pontok + 1;
-    } else {
-        alert("Sajnos rossz...");
-    }
-
-    sorszam = Math.floor(Math.random() * kerdesek.length);
-
-if (pontok < 10) { 
+        pontok = pontok + 1000;
+        
+        sorszam = Math.floor(Math.random() * kerdesek.length);
         kerdesBetoltes();
     } else {
-        document.getElementById("kerdes-szoveg").style.display = "none";
-        document.getElementById("b0").style.display = "none";
-        document.getElementById("b1").style.display = "none";
-        document.getElementById("b2").style.display = "none";
-        document.getElementById("b3").style.display = "none";
-        
-        document.getElementById("vege-uzenet").style.display = "block";
-        document.getElementById("eredmeny-szoveg").innerHTML = "Gratulálok! Elértél 10 pontot! 🦆";
+        alert("Sajnos rossz... A játék véget ért. A helyes válasz: " + kerdesek[sorszam][helyesValaszIndex + 1]);
+        jatekVege();
     }
 }
 
-kerdesBetoltes();
+function jatekVege() {
+    var kerdesSzoveg = document.getElementById("kerdes-szoveg");
+    if (kerdesSzoveg) kerdesSzoveg.style.display = "none";
+    
+    var gombok = document.querySelectorAll(".gombok");
+    gombok.forEach(g => g.style.display = "none");
+    
+    document.getElementById("vege-uzenet").style.display = "block";
+    document.getElementById("eredmeny-szoveg").innerHTML = "Vége a játéknak! Összesen " + pontok + " pontot szereztél! 🏆";
+}
+
+window.onload = function() {
+    kerdesBetoltes();
+};
